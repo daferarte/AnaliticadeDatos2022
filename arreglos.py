36fd26b7b372decfe652d758e006d159c332afd6
Daniel Arteaga <dfarteaga@unicesmag.edu.co>
2022-08-31 17:41:36 -0500

    agregando arrelgos

---
+++ arreglos.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+numeros=[1,2,3,4]
+
+arreglo=[1,"hola",3,"dfsdf",1,2,3,4]
+
+letras=['A','D','E','Z','b']
+
+arregloslice=arreglo[1:-1]
+
+letras.sort()
+
+arregloextendido=numeros
+
+arregloextendido.extend(letras)
+
+arregloextendido.index("A",)
+
+dir(numeros)
+
+print(letras)
